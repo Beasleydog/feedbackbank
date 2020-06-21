4b0402562da27a214fbd24020180a725c86d0c00
Beasleydog <49841434+Beasleydog@users.noreply.github.com>
2020-06-21 15:23:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,116 @@
+//search stuff
+var comments = [];
+fetch("https://script.google.com/macros/s/AKfycbygqcvjRdCgOUAC01x0CAoVKLG0R4eUpP1Gc7vZp6Ra2oMKJ2C-/exec").then(function(r){
+r.text().then(function(t){
+      document.getElementById("searchBar").style.display = "unset";
+document.getElementById("loading").remove();
+document.getElementById("s").innerHTML = t;
+  var sheets = [...document.getElementById("s").children];
+  var nta;
+  var temp = [];
+  var id= ""; 
+  var loc = "";
+  sheets.forEach(function(x){
+    id = x.firstChild.innerText;
+    x.firstChild.remove();
+    temp = [];
+    nta = [];
+    nta = [...x.children]
+    nta.forEach(function(c){
+      temp.push({t:c.innerText,location:Number(c.id)+1});
+    })
+    temp.splice(0,2);
+    comments.push({"id":id,"com":temp});
+  })
+})
+})
+document.getElementById("searchBar").oninput = function(){
+  if (document.getElementById("searchBar").value!=""){
+document.getElementById("results").innerHTML = "";
+  comments.forEach(function(sheet){
+    sheet.com.forEach(function(ec){
+      if (ec.t.toUpperCase().indexOf(document.getElementById("searchBar").value.toUpperCase())!=-1){
+        // if (ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())+1+document.getElementById("search").value.length]=="."||ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())+1+document.getElementById("search").value.length]==" "){
+        // if (ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())-1]==" "){
+        var tempResult = document.createElement("tr");
+        
+        var tempResultText=document.createElement("td");
+        tempResultText.onclick = function(){
+          fakeNotif("Comment has been copied to your clipboard!");
+
+          document.getElementById("searchBar").value = tempResultText.innerText;
+          document.getElementById("searchBar").select();
+          document.execCommand("copy");
+                    document.getElementById("searchBar").value = "";
+
+            document.getElementById("searchBox").style.display = "none";
+  document.getElementById("sheet").src ="https://docs.google.com/spreadsheets/d/1KTl9zl3RlDZ03o-sKvx_jN1FelS0UVIcGoDdbNqBVbM/edit?rm=minimal#gid="+sheet.id+"&range=B"+ec.location;
+    document.getElementById("results").innerHTML = "";
+
+        }
+        tempResultText.innerHTML= ec.t.replace(new RegExp(document.getElementById("searchBar").value, "gi"),"<b>"+document.getElementById("searchBar").value+"</b>");
+tempResultText.style.border="#6aa84f4f solid";
+tempResultText.style.padding="5px 5px";
+tempResultText.style.borderRadius="5px";
+tempResultText.style.marginBottom="15px";
+tempResultText.style.maxWidth="500px";
+tempResultText.style.overflow="hidden";
+        tempResult.appendChild(tempResultText);
+        document.getElementById("results").appendChild(tempResult);
+        
+        // }
+        // }
+      }
+    })
+  })
+  }else{
+    document.getElementById("results").innerHTML = "";
+
+  }
+}
+
+
+
+function fakeNotif(a) {
+    var notif = document.createElement("popupbody");
+    notif.style.all = "initial";
+    notif.style.backgroundColor = "#f5f7fa";
+    notif.style.boxShadow = "2px 2px 6px black";
+    notif.style.color = "black";
+    notif.style.fontSize = "14px";
+    notif.style.borderRadius = "2px";
+    notif.style.width = "180px";
+    notif.style.paddingLeft = "12px";
+    notif.style.height = "60px";
+    notif.style.position = "fixed";
+    notif.style.display = "block";
+    notif.style.borderTop = "6px solid #e67d0f";
+    notif.style.right = "-200px";
+    notif.style.lineHeight = "1rem";
+    notif.style.transition = "right 0.5s ease 0s";
+    notif.style.fontFamily = "none";
+    notif.style.zIndex = "999999999999999999999999999999999";
+    notif.style.top = "10px";
+    document.body.appendChild(notif);
+    var text = document.createElement("popuptext");
+    text.innerText = a;
+    text.style.fontWeight = "600";
+    text.style.position = "absolute";
+    text.style.top = "5px";
+    notif.appendChild(text);
+    setTimeout(function () {
+      notif.style.right = "-2px";
+    }, 10);
+    setTimeout(function () {
+      notif.style.right = "-200px";
+      setTimeout(function () {
+        notif.remove();
+      }, 500);
+    }, 5000);
+  }
+
+
+
 setTimeout(function(){
   document.getElementById("alert").style.opacity="1";
 
@@ -5,12 +118,18 @@ setTimeout(function(){
 document.getElementById("comment").onclick = function(){
   document.getElementById("addComment").style.display = "block";
     document.getElementById("url").value = document.getElementById("sheet").src;
-
 }
+document.getElementById("search").onclick = function(){
+  document.getElementById("searchBox").style.display = "block";
+}
+
+
 document.getElementById("close").onclick = function(){
   document.getElementById("addComment").style.display = "none";
 }
-
+document.getElementById("closes").onclick = function(){
+  document.getElementById("searchBox").style.display = "none";
+}
 
 document.getElementById("ok").onclick = function(){
     document.getElementById("alert").style.opacity="0";
