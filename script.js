83754b222986ab9a68c35c9c81463cb7ddecb695
Beasleydog <49841434+Beasleydog@users.noreply.github.com>
2020-07-20 18:26:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//mobile no no
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))){
+    document.getElementById("alert").innerHTML = `
+ <h1>
+        Welcome to The Feedback Bank!
+      </h1>
+<br>
+<br>
+<br>
+Please view on a computer!
+`;
+    document.getElementById("alert").style.width="95%";
+
+setTimeout(function(){
+  document.getElementById("alert").style.opacity="1";
+
+},5)
+}else{
+
+
 //analytics stuff
 if (location.href=="https://www.thefeedbackbank.com/"){
 var newUser = new FormData();
@@ -49,7 +70,7 @@ document.getElementById("searchBar").oninput = function(){
 document.getElementById("results").innerHTML = "";
   comments.forEach(function(sheet){
     sheet.com.forEach(function(ec){
-      if (document.getElementById("results").children.length<10){
+      if (document.getElementById("results").children.length<30){
       if (ec.t.toUpperCase().indexOf(searchTerms[0].toUpperCase())!=-1){
         // if (ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())+1+document.getElementById("search").value.length]=="."||ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())+1+document.getElementById("search").value.length]==" "){
         // if (ec[ec.toUpperCase().indexOf(document.getElementById("search").value.toUpperCase())-1]==" "){
@@ -193,6 +214,27 @@ Additional comments added to multiple sheets.
       <br>
         <div id="ok">
         OK
+      </div>`,`  <h1>
+        Welcome to The Feedback Bank!
+      </h1>
+      <br>
+      Click <a href="https://www.youtube.com/watch?v=Y8U2-l4Sy-c&feature=youtu.be" target="_blank">Here</a> for a 3-minute video on how to use the <a href="https://chrome.google.com/webstore/detail/feedback-finder/keljfallljoncbahaaibdjdbbffhgcip" target="_blank">Feedback Finder Chrome Extension</a>. 
+      <br>
+      <br>
+      <br>Search, copy/paste, and personalize comments without leaving your classroom/email.
+        <div id="ok">
+        OK
+      </div>`,`  <h1>
+        Welcome to The Feedback Bank!
+      </h1>
+<br>
+
+      Over <b>100</b> new comments have been added to the bank. <br><br>Categories include Kudos, Academic Integrity, Engagement, Grammar and more.
+<br>
+<br>
+<img src="https://cdn.pixabay.com/photo/2016/11/15/07/55/feedback-1825508_960_720.jpg" style="width:30%">
+        <div id="ok">
+        OK
       </div>`]
 
 setTimeout(function(){
@@ -327,6 +369,9 @@ document.getElementById("theme").onclick = function(){
 document.getElementById("leg").onclick = function(){
   document.getElementById("sheet").src = "https://docs.google.com/spreadsheets/d/1KTl9zl3RlDZ03o-sKvx_jN1FelS0UVIcGoDdbNqBVbM/edit?rm=minimal#gid=360926024";
 }
+  document.getElementById("kud").onclick = function(){
+  document.getElementById("sheet").src = "https://docs.google.com/spreadsheets/d/1KTl9zl3RlDZ03o-sKvx_jN1FelS0UVIcGoDdbNqBVbM/edit?rm=minimal#gid=242421084";
+}
 document.getElementById("res").onclick = function(){
   document.getElementById("sheet").src = "https://docs.google.com/spreadsheets/d/1KTl9zl3RlDZ03o-sKvx_jN1FelS0UVIcGoDdbNqBVbM/edit?rm=minimal#gid=112596641";
 }
@@ -344,4 +389,5 @@ document.getElementById("meme").onclick = function(){
 }
 document.getElementById("strat").onclick = function(){
   document.getElementById("sheet").src = "https://docs.google.com/spreadsheets/d/1KTl9zl3RlDZ03o-sKvx_jN1FelS0UVIcGoDdbNqBVbM/edit?rm=minimal#gid=471291255";
+}
 }
